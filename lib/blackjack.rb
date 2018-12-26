31b6dc1546b430eb36172186c7c08d0117a0c466
Heloise Taillet <ht2394@columbia.edu>
2018-12-26 05:00:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -22,8 +22,9 @@ def get_user_input
   input = gets.chomp
 end
 
-def end_game
+def end_game(total)
   # code #end_game here
+  puts "Sorry, you hit #{total}. Thanks for playing!"
 end
 
 def initial_round
